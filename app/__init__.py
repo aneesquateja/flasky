1c3c54fd43ce13da03fec23da59e6f2f84ca7366
Anees Quateja <aneesq87@gmail.com>
2024-10-31 15:32:31 -0700

    finished with wave_6

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,17 @@
 from flask import Flask
 from .routes.cat_routes import cats_bp
 from .db import db, migrate
-from .models import cat
+from app.models.cat import Cat
+import os
 
-def create_app():
+def create_app(config=None):
     # __name__ stores the name of the module we're in
     app = Flask(__name__)                                           # instance of flask
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False            # any modifications, could be noisy, we dont keep tracks of it, set it to false
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/flasky_development'     #points to our db "flasky_dev".
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')    #points to our db "flasky_dev".
+    if config:
+        app.config.update(config)
 
     db.init_app(app)            # follow doc, db has function init_app, put (flask app) in it
     migrate.init_app(app, db)   # migrate has init_app function and pass (app, db ) in it
---
+++ tests/test_cat_routes.py
@@ -0,0 +1,38 @@
+def test_get_all_cats_with_no_records(client):
+    #act
+    response = client.get("/cats")
+    response_body = response.get_json()
+    
+    #assert
+    assert response.status_code == 200
+    assert response_body == []
+
+def test_get_one_cat_succeeds(client, two_saved_cats):
+    response = client.get("cats/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name" : "Luna",
+        "color" : "ash",
+        "personality": "Tomato Queen"
+    }
+
+def test_create_one_cat_in_empty_db(client):
+    response = client.post("/cats", json={
+        "name" : "Garfield",
+        "color" : "ornage",
+        "personality" : "Likes Lasagna, hates Mondays"
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 201
+    assert response_body == {
+        "id" : 1,
+        "name" : "Garfield",
+        "color" : "ornage",
+        "personality" : "Likes Lasagna, hates Mondays"
+    }
+
+    
